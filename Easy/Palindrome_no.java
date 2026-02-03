ba1cb225ec75cfff12836e1ec08c3b9857db4433
CRASHs4aint <samratkumarsahu228@gmail.com>
2026-02-03 19:30:03 +0530

    update 03/2/26

---
+++ Easy/Palindrome_no.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+    int original=x;
+    int reverse=0;
+    
+    
+    while (x != 0) {
+            
+            reverse = reverse * 10 + (x%10);
+            x /= 10;
+        }
+        if(original!=reverse || original<0){
+            return false;
+        
+        }
+        else{
+             return true;
+        }
+    
+        
+    }
+}
