cfe6afc87d8231b3e0526729f5c89059a3dbb664
CRASHs4aint <samratkumarsahu228@gmail.com>
2026-02-05 21:58:19 +0530

    update 05/2/2026

---
+++ Easy/Jwels_Stone.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            char stoneChar = stones.charAt(i);
+
+            for (int j = 0; j < jewels.length(); j++) {
+                char jewelChar = jewels.charAt(j);
+                if (stoneChar == jewelChar) {
+                    count++;
+                }
+
+            }
+        }
+        return count;
+    }
+}
